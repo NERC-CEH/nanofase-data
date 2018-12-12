30b4b33133abd1afce84ea443f7236afa5a18ebd
Sam Harrison <samharrison.xg@gmail.com>
2018-12-12 15:17:12 +0000

    Adding data compilation script.

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # NanoFASE Data Tools
 
 Collection of scripts for parsing data related to the [NanoFASE model](https://github.com/nerc-ceh/nanofase).
+
+## `input-data-compilation.py`
+Compile input data into one big JSON file, for subsequent conversion to NetCDF.
+
+Config options:
+- `flow_dir_raster`: Path to the flow direction raster for the desired catchment. Must be in ESPG:27700 projection (OSGB). The bounds of this raster will be used to clip all of the other data. File must be a valid raster file, openable by [Rasterio](https://github.com/mapbox/rasterio).
+- `runoff`: CSV file of runoff with column headers `yr,month,day,easts,norths,QF,SF`. Easts and norths are OSGB grid references for the *centre* of the grid cell and QF and SF are quickflow and slowflow, as output from HMF run with CERF.
+- `atmospheric_dry_depo_dir`: Directory to the atmospheric dry deposition data. Data should be one file per time step and have the file name `LE-Sofia-UK_drydepo_dayval_{t}.tif`, where `{t}` is the time step number. Must be in ESPG:27700 projection (OSGB).
+- `atmospheric_wet_depo_dir`: Directory to the atmospheric wet deposition data. Data should be one file per time step and have the file name `LE-Sofia-UK_wetdepo_dayval_{t}.tif`, where `{t}` is the time step number. Must be in ESPG:27700 projection (OSGB).
+- `sources`: Path to the sources data .txt file with column headers `ISO3;Nanomaterial;Compartment;Form;SourceType;Longitude;Latitude;SumOfLocal_emission_ton`. Values must be semicolon separated. Longitude and latitude are in degrees.
+- `soil_texture_dir`: Directory to the sand, silt, clay and coarse-frag raster files. File names must follow format `{0}-content_osgb_5000m.tif`, where `{0}` is iether sand, silt, clay or coarse-frag. Must be in ESPG:27700 projection (OSGB).
+- `bulk_density`: Path to bulk density raster. Must be in ESPG:27700 projection (OSGB) and be a valid raster file, openable by Rasterio.
+- `c-factor`, `ls-factor` and `p-factor`: Path to the RUSLE C-factor, LS-factor and P-factor rasters. Must be in ESPG:27700 projection (OSGB) and be a valid raster file, openable by Rasterio.
+- `precipitation_dir`: Directory to the precipitation data. Data should be one file per time step and have the file name `Lrainfall_5km_2015-{t}.tif`, where `{t}` is the time step number. Must be in ESPG:27700 projection (OSGB).
+- `output_file`: Where should the final JSON file be placed?
---
+++ input-data-compilation.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+"""Compiling different data sources into JSON file (for conversion to NetCDF)
+for NanoFASE model."""
+import rasterio
+from rasterio.features import rasterize
+import yaml
+import datautils as du
+import json
+
+# Get the config options from the YAML file
+with open("config.yaml", 'r') as stream:
+    try:
+        config = yaml.load(stream)
+    except yaml.YAMLError as exc:
+        print(exc)
+
+print('Specified config options:')
+for opt, val in config.items():
+    print('\t{0}: {1}'.format(opt, val))
+
+print('\nLoading flow direction...')
+# Load flow direction raster and save the min/max dimensions
+rs = rasterio.open(config['flow_dir_raster'])
+xmin, xmax = int(rs.bounds.left), int(rs.bounds.right)
+ymin, ymax = int(rs.bounds.bottom), int(rs.bounds.top)
+
+print('Generating grid structure and linking grid cells/river reaches...')
+data_dict = du.generate_grid_from_flow_dir(rs)
+
+print('Parsing runoff data...')
+data_dict = du.parse_runoff_data(config['runoff'], data_dict)
+
+print('Parsing atmospheric data...')
+data_dict = du.parse_atmospheric_data(config['atmospheric_dry_depo_dir'], config['atmospheric_wet_depo_dir'], data_dict, config['timesteps'])
+
+# Source data parsing is horrendously inefficient at the moment!
+print('Parsing source data...')
+data_dict = du.parse_source_data(config['sources'], data_dict)
+
+# Soil texture. LUCAS interpolated by (https://esdac.jrc.ec.europa.eu/content/topsoil-physical-properties-europe-based-lucas-topsoil-data)
+# and resampled to OSGB
+print('Parsing soil texture data...')
+data_dict = du.parse_soil_texture_data(config['soil_texture_dir'], data_dict)
+
+# Bulk density [g/cm3 = T/m3]
+print('Parsing bulk density data...')
+data_dict = du.parse_bulk_density_data(config['bulk_density'], data_dict)
+
+# USLE C-factor. This resampled raster (from ESDAC dataset) isn't that realistic - there are
+# too many data gaps for urban areas, which is an artefact of the urban areas being amplified
+# upon conversion. In the future, it might be worth proportioning this relative to different
+# land use classes in each cell (see the C-factor paper)
+print('Parsing C-factor data...')
+data_dict = du.parse_cfactor_data(config['c-factor'], data_dict)
+
+# USLE LS-factor. Resampled raster (from ESDAC dataset) from 100m to 5x5 km.
+print('Parsing LS-factor data...')
+data_dict = du.parse_lsfactor_data(config['ls-factor'], data_dict)
+
+# USLE P-factor (support practice). Resampled raster (from ESDAC dataset) from 1 km to 5 km.
+# Same missing urban data issues as C-factor. Set these to 1 (no support practice) from the moment.
+print('Parsing P-factor data...')
+data_dict = du.parse_pfactor_data(config['p-factor'], data_dict)
+
+print('Parsing precipitation data...')
+data_dict = du.parse_precipitation_data(config['precipitation_dir'], data_dict, config['timesteps'])
+
+# Filling in the gaps
+print('Filling the gaps...')
+data_dict["dimensions"] = { "d": 2, "state": 7, "form": 4, "n": 5 }
+for grid_cell_ref, grid_cell in data_dict.items():
+    if grid_cell_ref not in ["dimensions", "grid_dimensions[d]"]:
+        x = grid_cell['x']
+        y = grid_cell['y']
+        data_dict[grid_cell_ref]["dimensions"] = { "r": 1 }
+        data_dict[grid_cell_ref]["type"] = 1
+        data_dict[grid_cell_ref]["slope"] = 0.0005
+        data_dict[grid_cell_ref]["SoilProfile_{0}_{1}_1".format(x, y)].update({
+            "n_soil_layers" : 4,
+            "WC_sat" : 0.8,
+            "WC_FC" : 0.5,
+            "K_s" : 1e-6,
+            "usle_K" : 0.032,
+            "usle_area_hru" : 0.1,
+            "usle_L_ch" : 500,
+            "distribution_sediment[s]" : [ 50, 30, 10, 7, 3 ],
+            "SoilLayer_1" : {
+                "depth" : 0.1
+            },
+            "SoilLayer_2" : {
+                "depth" : 0.1
+            },
+            "SoilLayer_3" : {
+                "depth" : 0.1
+            },
+            "SoilLayer_4" : {
+                "depth" : 0.1
+            }
+        })
+        data_dict[grid_cell_ref]["demands"] = "file::demands-example.json"
+        data_dict[grid_cell_ref]["reach_types[r]"] = [1]
+        for i in range(1,data_dict[grid_cell_ref]["n_river_reaches"] + 1):
+            reach_ref = "RiverReach_{0}_{1}_{2}".format(x,y,i)
+            data_dict[grid_cell_ref][reach_ref]["BedSediment"] = "file::bed-sediment-compact.json"
+            data_dict[grid_cell_ref][reach_ref]["beta_res"] = 0.000001
+            data_dict[grid_cell_ref][reach_ref]["alpha_res"] = 0.001
+            data_dict[grid_cell_ref][reach_ref]["slope"] = 0.0005
+            if "inflows[in][river_reach_ref]" in data_dict[grid_cell_ref][reach_ref]:
+                data_dict[grid_cell_ref][reach_ref]["dimensions"] = { "in": len(grid_cell[reach_ref]['inflows[in][river_reach_ref]']) }
+        # Remove some stuff
+        if 'inflows' in data_dict[grid_cell_ref]: 
+            del data_dict[grid_cell_ref]['inflows']
+
+# Put all this in to a file    
+with open(config['output_file'], "w") as f:
+    json.dump(data_dict, f, indent=4)
+
+print('Compiled data saved to {0}'.format(config['output_file']))
